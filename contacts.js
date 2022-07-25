e1d3201c1586ef8edcda81ef1faf87003032a9a5
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-25 21:24:11 +0700

    Add files via upload

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
-// File System
 const fs = require("fs");
+const chalk = require("chalk");
+const validator = require("validator");
 
 // membuat folder dara jika belum ada
 const dirPath = "./data";
@@ -20,15 +21,28 @@ const simpanContact = (nama, email, noHP) => {
   // Cek Duplikat
   const duplikat = contacts.find((contact) => contact.nama === nama);
   if (duplikat) {
-    console.log("Contact sudah terdaftar, gunakan nama lain!");
+    console.log(chalk.red.inverse.bold("Contact sudah terdaftar, gunakan nama lain!"));
     return false;
+  }
+
+  // Cek Email
+  if (email) {
+    if (!validator.isEmail(email)) {
+      console.log(chalk.red.inverse.bold("Email tidak valid!"));
+      return false;
+    }
+  }
+
+  // Cek Nomor HandPhone
+  if (!validator.isMobilePhone(noHP, "id-ID")) {
+    console.log(chalk.red.inverse.bold("Nomor HP tidak valid!"));
   }
 
   contacts.push(contact);
 
   fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
 
-  console.log("Terimakasih sudah memasukkan data.");
+  console.log(chalk.green.inverse.bold("Terimakasih sudah memasukkan data."));
 };
 
 // memanggil property nama, email, noHP di file app.js
